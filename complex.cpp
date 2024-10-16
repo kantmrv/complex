115f5908566b4a5624ac26a541fedbab7177542e
Alexandr <123596410+kantmrv@users.noreply.github.com>
2024-10-16 22:06:37 +0300

    Update complex.cpp

--- complex.cpp
+++ complex.cpp
@@ -3,20 +3,22 @@
 
 int main() {
 	complex a = { 3, 4 }, b = { 2, 1 };
-	
+
+	std::cout << "1 = " << (complex(1)) << std::endl;
 	std::cout << "a = " << a << std::endl;
 	std::cout << "Re(a) = " << a.Re() << std::endl;
 	std::cout << "Im(a) = " << a.Im() << std::endl;
 	std::cout << "|a| = " << a.Abs() << std::endl;
 	std::cout << "~a = " << ~a << std::endl;
-	std::cout << "a * ~a = " << a * ~a << std::endl;
+	std::cout << "a * ~a = " << (a * ~a) << std::endl;
 	std::cout << "a = " << a << ", (a == a) = " << int(a == a) << std::endl;
 	std::cout << "a = " << a << ", b = " << b << ", (a == b) = " << int(a == b) << std::endl;
-	std::cout << "a = " << a << ", 1 = " << complex(1) << ", a + 1 = " << a + 1 << std::endl;
-	std::cout << "a = " << a << ", b = " << b << ", a + b = " << a + b << std::endl;
-	std::cout << "a = " << a << ", b = " << b << ", a - b = " << a - b << std::endl;
-	std::cout << "a = " << a << ", b = " << b << ", a * b = " << a * b << std::endl;
-	std::cout << "a = " << a << ", b = " << b << ", a / b = " << a / b << std::endl;
+	std::cout << "a = " << a << ", 1 + a = " << (1 + a) << std::endl;
+	std::cout << "a = " << a << ", b = " << b << ", a + b = " << (a + b) << std::endl;
+	std::cout << "a = " << a << ", b = " << b << ", a - b = " << (a - b) << std::endl;
+	std::cout << "a = " << a << ", b = " << b << ", a * b = " << (a * b) << std::endl;
+	std::cout << "a = " << a << ", b = " << b << ", a / b = " << (a / b) << std::endl;
+	std::cout << "a = " << a << ", a ^ 7 = " << (a ^ 7) << std::endl;
 
 	return 0;
 }
